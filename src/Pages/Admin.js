92fed7d5789d66c00784edef6322dc7712b6cf22
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2022-06-17 19:56:23 -0400

    Add item to database and edit item from database functionallity implemented

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -7,16 +7,22 @@ import {
     signInWithEmailAndPassword
 } from "firebase/auth"
 import { db } from '../Firebase-config'
-import { collection, addDoc , getDocs} from 'firebase/firestore'
+import { collection, addDoc , getDocs, updateDoc, doc, setDoc} from 'firebase/firestore'
+import { useGlobalContext } from '../context'
 
 const Admin = () => {
+    const {coffeeData} = useGlobalContext()
 
     const [newsModal, setNewsModal] = useState(false)
+    const [editNewsModal, setEditNewsModal] = useState(false)
     const [discoveryModal, setDiscoveryModal] = useState(false)
     const [loginEmail, setLoginEmail] = useState('')
     const [loginPassword, setLoginPassword] = useState('')
     const [user, setUser] = useState({})
+
+    // DATA REFERENCE
     const dataRef = collection(db, "coffeeData")
+    // const dataRef = doc(collection(db, "coffeeData"))
 
     onAuthStateChanged(auth, (currentUser) =>{
         setUser(currentUser)
@@ -39,6 +45,7 @@ const Admin = () => {
     }
 
     // ADD NEW POST TO DATABASE
+    const [ident, setIdent] = useState('')
     const [type, setType] = useState('')
     const [id, setId] = useState('')
     const [index, setIndex] = useState('')
@@ -63,6 +70,13 @@ const Admin = () => {
     }
     dataLenght()
 
+    // const dataId = async() =>{
+    //     const data = await getDocs(dataRef)
+    //     setIdent((data.docs.map((doc) =>({...doc.id}))))
+    //     console.log(ident)
+    // }
+    // dataId()
+
     const clearInputs = ()=>{
         setType('')
         setId('')
@@ -84,7 +98,7 @@ const Admin = () => {
     }
 
     const createPost = async () =>{
-        await addDoc(dataRef, {
+        await setDoc(doc(db,"coffeeData",id), {
             type: type,
             id: id,
             index: index,
@@ -92,11 +106,12 @@ const Admin = () => {
             title: title,
             short: short,
             descript: descrip,
-            date: date
+            date: date,
         })
     }
+
     const createDiscovery = async () =>{
-        await addDoc(dataRef, {
+        await setDoc(doc(db,"coffeeData",id), {
             type: type,
             id: id,
             index: index,
@@ -116,6 +131,39 @@ const Admin = () => {
         })
     }
 
+    // EDIT ITEM
+    const [editItem, setEditItem] = useState('')
+
+    const editItemData = () => {
+        coffeeData.map((item)=>{
+            if(item.id === editItem){
+                setType(item.type)
+                setId(item.id)
+                setIndex(item.index)
+                setImg(item.image)
+                setTitle(item.title)
+                setShort(item.short)
+                setDescript(item.descript)
+                setDate(item.date)
+                // console.log(item)
+            }
+        })
+    }
+
+
+    const updateCoffee = async () =>{
+        const someDocRef = doc(db, "coffeeData", id)
+        await updateDoc (someDocRef,{
+            type: type,
+            id: id,
+            index: index,
+            image: img,
+            title: title,
+            short: short,
+            descript: descrip,
+            date: date
+        })
+    }
 
     return (
         <div className="admin__page">
@@ -138,6 +186,29 @@ const Admin = () => {
                 >
                     Sign Out
                 </button>
+            </div>
+
+            {/* EDIT CONTAINER */}
+            <div className="edit__cont">
+                <h1>Edit News or Discovery</h1>
+                <div className="edit__cont--form">
+                    <input 
+                        type="text" 
+                        className="edit__link--input" 
+                        placeholder='Paste ID here'
+                        onChange={(e)=> setEditItem(e.target.value)}
+                    />
+                    <button 
+                        className="edit__link--btn"
+                        onClick={()=>{
+                            editItemData()
+                            setEditNewsModal(true)
+                        }}
+                    >
+                        Edit File
+                    </button>
+                </div>
+                
             </div>
 
             <div className="admin__modal">
@@ -196,6 +267,68 @@ const Admin = () => {
                                 <button
                                     className='close__form--btn'
                                     onClick={()=> setNewsModal(false)}
+                                >
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Edit --------NEWS/TREDNS FORM 
+                ********************/}
+                <div className={editNewsModal ? "blog__form" : "blog__form hide"}>
+                    <div className='form'>
+                        <div className="form__left form__col">
+                            <input 
+                                placeholder='Type'
+                                onChange={(e)=> setType(e.target.value)}
+                                value={type}
+                            />
+                            <input 
+                                placeholder='Id'
+                                onChange={(e)=> setId(e.target.value)}
+                                value={id}
+                            />
+                            <input 
+                                placeholder='Image Link'
+                                onChange={(e)=> setImg(e.target.value)}
+                                value={img}
+                            />
+                            <input 
+                                placeholder='Title'
+                                onChange={(e)=> setTitle(e.target.value)}
+                                value={title}
+                            />
+                            <input 
+                                placeholder='Short'
+                                onChange={(e)=> setShort(e.target.value)}
+                                value={short}
+                            />
+                        </div>
+                        <div className="form__right form__col">
+                            <textarea name="textare" id="" cols="30" rows="10" placeholder='Descript'
+                            onChange={(e)=> setDescript(e.target.value)}
+                            value={descrip}
+                            ></textarea>
+                            <input 
+                                placeholder='Date month/day/year'
+                                onChange={(e)=> setDate(e.target.value)} 
+                                value={date}
+                            />
+                            <div className="form__btns">
+                                <button 
+                                    className='form__btn pink__btn'
+                                    onClick={()=>{
+                                        updateCoffee()
+                                        clearInputs()
+                                    }}
+                                    >
+                                    Update Trend/News
+                                </button>
+                                <button
+                                    className='close__form--btn'
+                                    onClick={()=> setEditNewsModal(false)}
                                 >
                                     Close
                                 </button>
@@ -319,7 +452,7 @@ const Admin = () => {
                 </div>
             </div>
 
-
+            {/* LOGIN FORM */}
             <div className={user ? "login__cont close" : "login__cont"}>
                 <input 
                     type="email" 
